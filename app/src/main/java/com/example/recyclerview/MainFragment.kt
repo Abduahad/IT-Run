3034e2127e4dc88ffc499ecbb92dd949a3c3d781
Abduahad <fayzulloev.a@eskhata.tj>
2024-03-11 18:52:24 +0500

    Migration from RecyclerView to LinearLayout

--- app/src/main/java/com/example/recyclerview/MainFragment.kt
+++ app/src/main/java/com/example/recyclerview/MainFragment.kt
@@ -6,17 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainFragment : Fragment(), View.OnClickListener {
-    private lateinit var recyclerView: RecyclerView
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-
-    }
+    private lateinit var linearLayout: LinearLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,20 +24,54 @@ class MainFragment : Fragment(), View.OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        val adapter:MyAdapter = MyAdapter(getHistory(),this)
-        recyclerView.adapter = adapter
+        linearLayout= view.findViewById<LinearLayout>(R.id.linearLayout)
+        initItems(getHistory())
     }
 
-    fun getHistory():List<Any>{
-        val list:ArrayList<Any> = ArrayList()
-        list.add("29.02.2024")
+    private fun initItems( list:List<HistoryBodyData>){
+        list.forEach {data->
+            val itemView = getItemView()
+            bindItem(itemView,data)
+            linearLayout.addView(itemView)
+        }
+    }
+
+    fun getHistory():List<HistoryBodyData>{
+        val list:ArrayList<HistoryBodyData> = ArrayList()
         list.add(HistoryBodyData(R.drawable.ic_tcell,HistoryStatuses.SUCCES,"Tcell","92 616 44 36","10 TJS"))
         list.add(HistoryBodyData(R.drawable.ic_tcell,HistoryStatuses.REJECT,"Tcell","(92 777 77 77)","100 TJS"))
         list.add(HistoryBodyData(R.drawable.ic_tcell,HistoryStatuses.IN_PROGRESS,"Tcell","92 111 11 11","50 TJS"))
         return list
     }
+
+    private fun getItemView():View{ return  LayoutInflater.from(requireContext()).inflate(R.layout.list_item, null, false) }
+
+    private fun bindItem(itemView:View, data: HistoryBodyData){
+        val textViewTitle: TextView = itemView.findViewById<TextView>(R.id.textViewTitle)
+        val textViewBody: TextView = itemView.findViewById<TextView>(R.id.textViewBody)
+        val textViewSum: TextView = itemView.findViewById<TextView>(R.id.textViewSum)
+        val imageViewStatus: ImageView =itemView.findViewById(R.id.imageViewStatus)
+        val imageViewService: ImageView = itemView.findViewById(R.id.imageViewService)
+        textViewTitle.text=data.title
+        textViewBody.text=data.body
+        textViewSum.text=data.endText
+        imageViewService.setImageResource(data.icon)
+        when(data.status){
+            HistoryStatuses.IN_PROGRESS->{
+                imageViewStatus.setImageResource(R.drawable.ic_status_proccess)
+            }
+            HistoryStatuses.SUCCES->{
+                imageViewStatus.setImageResource(R.drawable.ic_status_done)
+            }
+            HistoryStatuses.REJECT->{
+                imageViewStatus.setImageResource(R.drawable.ic_status_rejected)
+            }
+            else->{}
+        }
+        itemView.setOnClickListener(this)
+    }
+
+
 
     override fun onClick(v: View?) {
         context?.let {
